f719c139b86db07ef3901000451645246c42c9bf
Oleksii Tymchenko <o.tymchenko@softpositive.com>
2020-02-09 22:38:53 +0200

    added controller for jquery version

---
+++ src/Kpi.Hotels.Api/Controllers/ClientsJqueryController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Kpi.Hotels.Dataclient.Context;
+using Kpi.Hotels.Dataclient.Context.Models;
+
+namespace kpi_hotels_api.Controllers
+{
+    [Route("gui/[controller]")]
+    public class ClientsJqueryController : Controller
+    {
+        private readonly HotelsContext _context;
+
+        public ClientsJqueryController(HotelsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ClientsJquery
+        [Route("/")]
+        [Route("Index")]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clients.ToListAsync());
+        }
+
+        // GET: ClientsJquery/Details/5
+        [Route("Details")]
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // GET: ClientsJquery/Create
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientsJquery/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Route("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,Address,Nationality,Email")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                client.Id = Guid.NewGuid();
+                _context.Add(client);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // GET: ClientsJquery/Edit/5
+        [Route("Edit")]
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        // POST: ClientsJquery/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Route("Edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,FirstName,LastName,PhoneNumber,Address,Nationality,Email")] Client client)
+        {
+            if (id != client.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
+        }
+
+        // GET: ClientsJquery/Delete/5
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // POST: ClientsJquery/Delete/5
+        [Route("Delete")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientExists(Guid id)
+        {
+            return _context.Clients.Any(e => e.Id == id);
+        }
+    }
+}

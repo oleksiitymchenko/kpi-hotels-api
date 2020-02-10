2bacb74ee672ec4cd94dae4cd1c8854a5d83abd1
Oleksii Tymchenko <o.tymchenko@softpositive.com>
2020-02-10 23:30:54 +0200

    jquery views

--- src/Kpi.Hotels.Api/Controllers/ClientsJqueryController.cs
+++ src/Kpi.Hotels.Api/Controllers/ClientsJqueryController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Kpi.Hotels.Dataclient.Context;
-using Kpi.Hotels.Dataclient.Context.Models;
 
 namespace kpi_hotels_api.Controllers
 {
@@ -53,24 +52,6 @@ namespace kpi_hotels_api.Controllers
             return View();
         }
 
-        // POST: ClientsJquery/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Route("Create")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PhoneNumber,Address,Nationality,Email")] Client client)
-        {
-            if (ModelState.IsValid)
-            {
-                client.Id = Guid.NewGuid();
-                _context.Add(client);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(client);
-        }
-
         // GET: ClientsJquery/Edit/5
         [Route("Edit")]
         public async Task<IActionResult> Edit(Guid? id)
@@ -84,42 +65,6 @@ namespace kpi_hotels_api.Controllers
             if (client == null)
             {
                 return NotFound();
-            }
-            return View(client);
-        }
-
-        // POST: ClientsJquery/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Route("Edit")]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,FirstName,LastName,PhoneNumber,Address,Nationality,Email")] Client client)
-        {
-            if (id != client.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(client);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ClientExists(client.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
             }
             return View(client);
         }
@@ -141,18 +86,6 @@ namespace kpi_hotels_api.Controllers
             }
 
             return View(client);
-        }
-
-        // POST: ClientsJquery/Delete/5
-        [Route("Delete")]
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var client = await _context.Clients.FindAsync(id);
-            _context.Clients.Remove(client);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool ClientExists(Guid id)

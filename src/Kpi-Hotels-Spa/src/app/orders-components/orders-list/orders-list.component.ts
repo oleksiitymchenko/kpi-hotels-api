b6bcf57a25b63ffe55d42299ca2e5fd7db25f6ca
Oleksii Tymchenko <o.tymchenko@softpositive.com>
2020-02-16 15:56:29 +0200

    orders list

--- src/Kpi-Hotels-Spa/src/app/orders-components/orders-list/orders-list.component.ts
+++ src/Kpi-Hotels-Spa/src/app/orders-components/orders-list/orders-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/services/orders.service';
+import { order } from 'src/app/models/order';
 
 @Component({
   selector: 'app-orders-list',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: OrdersService) { }
+  public orderList = Array<order>();
+  private subscription;
 
   ngOnInit(): void {
+    console.log("INIT");
+    this.subscription = this.service.getAllOrders().subscribe(
+      x => {
+        this.orderList = x;
+        console.log(x);
+      });
+  }
+
+  ngOnDestroy():void{
+    this.subscription.unsubscribe();
   }
 
 }
